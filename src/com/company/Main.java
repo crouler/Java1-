ab47085d16a63f71b3afa4597709ffff8c01c080
elma <belmaspi@yandex.ru>
2019-04-02 12:18:18 +0300

    Скопировал пустой проект

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+	// write your code here
+
+System.out.println("Hellow World");
+
+
+
+
+
+
+    }
+}
